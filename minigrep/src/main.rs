55fa875a189a0cc9f07ded967cd385a551afd8c9
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-12-19 17:38:39 -0500

    use Result<> for error handling

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|error| {
+        println!("Problem parsing arguments: {error}");
+        process::exit(1);
+    });
 }
 
 struct Config {
@@ -13,16 +17,15 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
-
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("Not enough arguments");
+            return Err("Not neough arguments");
         }
 
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config {query, file_path}
+        Ok(Config {query, file_path})
     }
 }
 
