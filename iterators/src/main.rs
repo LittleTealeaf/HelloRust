4dc5386045db0af24f245a4de759639a008f07ac
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-12-20 13:35:09 -0500

    notes for iterators

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -31,6 +31,37 @@ fn main() {
             println!("Got: {}", val);
         }
     }
+    {
+        #[derive(PartialEq, Debug)]
+        struct Shoe {
+            size: u32,
+            style: String,
+        }
+
+        fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+            shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+        }
+
+        let shoes = vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker")
+            },
+            Shoe {
+                size: 13,
+                style: String::from("sandal")
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot")
+            }
+        ];
+
+        let in_my_size = shoes_in_size(shoes, 10);
+        // function takes ownership of the shoes variable
+
+        println!("{:?}", in_my_size);
+    }
 }
 
 #[cfg(test)]
